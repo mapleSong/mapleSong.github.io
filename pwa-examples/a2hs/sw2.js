a52d4d46440186ac77ce34d081b22897be2d2653
songmingfeng <songmingfeng@360.cn>
2023-04-10 11:23:31 +0800

    update

--- pwa-examples/a2hs/sw2.js
+++ pwa-examples/a2hs/sw2.js
@@ -1,8 +1,8 @@
-self.addEventListener('install', function(e) {
-  console.log('install success')
+self.addEventListener('install', function (e) {
+  console.log('install success');
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   console.log(e.request.url);
   // e.respondWith(
   //   caches.match(e.request).then(function(response) {
